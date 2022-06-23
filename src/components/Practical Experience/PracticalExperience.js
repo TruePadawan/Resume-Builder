37fa83fccd1353818a6604853ae4e2aeabeaa219
Hermes <hermeschigoziri@gmail.com>
2022-06-23 21:13:15 +0100

    build up the practical experience section

--- src/components/Practical Experience/PracticalExperience.js
+++ src/components/Practical Experience/PracticalExperience.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { InputField } from "../InputField/InputField";
+import Button from "../Button/Button";
+import { InputField, TextArea } from "../InputField/InputField";
 import Section from "../Section/Section";
 
 export default class PracticalExp extends React.Component {
     render() {
         return (
             <Section sectionTitle="Practical Experience">
-                <form>
+                <form className="cv-form">
                     <div className="flex-row">
                         <InputField isRequired={true} label="Company" />
                         <InputField isRequired={true} label="Position" />
                     </div>
                     <div className="flex-row">
                         <InputField isRequired={true} label="From" type="date" />
-                        <InputField label="To" type="date" />
+                        <InputField label="To" placeholder="Year/Ongoing" />
                     </div>
+                    <TextArea label="Highlights" />
+                    <Button className="add-btn">Add</Button>
+                    <Button className="delete-btn">Delete</Button>
                 </form>
             </Section>  
         );
