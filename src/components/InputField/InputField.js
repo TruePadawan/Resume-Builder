39e557883a0e9f4463e8b092c708102cb1f1bda4
Hermes <hermeschigoziri@gmail.com>
2022-06-23 16:56:04 +0100

    Add props for setting value and placeholder

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "./inputField.css";
 
+/*
+    Input Field Props - isRequired, type, label, placeholder, value
+*/
 export class InputField extends React.Component {
     render() {
-        const inputType = this.props.inputType || "text";
+        const inputType = this.props.type || "text";
         const isInputRequired = this.props.isRequired;
-        const inputLabel = this.props.inputLabel;
+        const inputLabel = this.props.label;
+        const placeholder = this.props.placeholder || "";
+        const value = this.props.value || "";
 
         if (isInputRequired)
         {
             return (
                 <span className="inputField">
                     <label htmlFor={inputLabel}>{inputLabel}</label>
-                    <input id={inputLabel} type={inputType} required/>
+                    <input id={inputLabel} type={inputType} required placeholder={placeholder} defaultValue={value} />
                 </span>
             );
         }
         return (
           <span className="inputField">
             <label htmlFor={inputLabel}>{inputLabel}</label>
-            <input id={inputLabel} type={inputType} />
+            <input id={inputLabel} type={inputType} placeholder={placeholder} defaultValue={value} />
           </span>
         );
     }
@@ -29,20 +34,22 @@ export class TextArea extends React.Component {
     render() {
         const isRequired = this.props.isRequired;
         const label = this.props.label;
+        const placeholder = this.props.placeholder || "";
+        const value = this.props.value || "";
 
         if (isRequired)
         {
             return (
                 <span className="inputField">
                     <label htmlFor={label}>{label}</label>
-                    <textarea id={label} required/>
+                    <textarea id={label} required placeholder={placeholder} defaultValue={value} />
                 </span>
             );
         }
         return (
           <span className="inputField">
             <label htmlFor={label}>{label}</label>
-            <textarea id={label} />
+            <textarea id={label} placeholder={placeholder} defaultValue={value} />
           </span>
         );
     }
