3eae2cb494803597735320619d314d63dffef759
Hermes <hermeschigoziri@gmail.com>
2022-06-24 13:46:28 +0100

    add contact section to cv preview

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import Button from "./components/Button/Button";
 import Education from "./components/Education/Education";
 import GeneralInfo from "./components/General Information/GeneralInfo";
 import PracticalExp from "./components/Practical Experience/PracticalExperience";
+
+import PhoneImg from "./images/phone.png";
+import TwitterImg from "./images/twitter.png";
+import LinkedInImg from "./images/linkedin.png";
+import MailImg from "./images/mail.png";
+
 import "./css/App.css";
 import "./css/main.css";
 
@@ -33,9 +39,24 @@ class App extends Component {
                                 <p className="full-name">{fullName}</p>
                             </div>
                             <div className="general">
-                                <div className="contact">
-                                    
-                                </div>
+                                <ul className="contact">
+                                    <li>
+                                        <img src={PhoneImg} alt="phone" />
+                                        <span className="phone-number">XXXXXXXXXXX</span>
+                                    </li>
+                                    <li>
+                                        <img src={LinkedInImg} alt="linkedin" />
+                                        <span className="linkedin-url">XXXXXXXXXXX</span>
+                                    </li>
+                                    <li>
+                                        <img src={MailImg} alt="email" />
+                                        <span className="email">XXXXXXXXXXX</span>
+                                    </li>
+                                    <li>
+                                        <img src={TwitterImg} alt="twitter" />
+                                        <span className="twitter-url">XXXXXXXXXXX</span>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </section>
