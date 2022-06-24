266cce568edf0e73c047042a5b7e4a7c16c4b61e
Hermes <hermeschigoziri@gmail.com>
2022-06-24 22:07:17 +0100

    update inputField components

--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -7,27 +7,44 @@ import "./inputField.css";
 export class InputField extends React.Component {
     render() {
         const inputType = this.props.type || "text";
-        const isInputRequired = this.props.isRequired;
+        const isInputRequired = this.props.isRequired || false;
         const inputLabel = this.props.label;
-        const placeholder = this.props.placeholder || "";
-        const value = this.props.value || "";
-        const list = this.props.listID || "";
-        const dataList = this.props.list || "";
+        const placeholder = this.props.placeholder;
+        const value = this.props.value;
+        const list = this.props.listID;
+        const dataList = this.props.list;
+        const className = this.props.className;
 
         if (isInputRequired)
         {
             return (
-                <span className="inputField">
-                    <label htmlFor={inputLabel}>{inputLabel}</label>
-                    <input id={inputLabel} type={inputType} required placeholder={placeholder} defaultValue={value} list={list} />
-                    {dataList !== "" && dataList}
-                </span>
+              <span className="inputField">
+                <label htmlFor={inputLabel}>{inputLabel}</label>
+                <input
+                  className={className}
+                  id={inputLabel}
+                  type={inputType}
+                  required
+                  placeholder={placeholder}
+                  defaultValue={value}
+                  list={list}
+                  ref={this.props.compRef}
+                />
+                {dataList !== "" && dataList}
+              </span>
             );
         }
         return (
           <span className="inputField">
             <label htmlFor={inputLabel}>{inputLabel}</label>
-            <input id={inputLabel} type={inputType} placeholder={placeholder} defaultValue={value} />
+            <input
+              className={className}
+              id={inputLabel}
+              type={inputType}
+              placeholder={placeholder}
+              defaultValue={value}
+              ref={this.props.compRef}
+            />
             {dataList !== "" && dataList}
           </span>
         );
@@ -40,20 +57,34 @@ export class TextArea extends React.Component {
         const label = this.props.label;
         const placeholder = this.props.placeholder || "";
         const value = this.props.value || "";
+        const className = this.props.className;
 
         if (isRequired)
         {
             return (
-                <span className="inputField">
-                    <label htmlFor={label}>{label}</label>
-                    <textarea id={label} required placeholder={placeholder} defaultValue={value} />
-                </span>
+              <span className="inputField">
+                <label htmlFor={label}>{label}</label>
+                <textarea
+                  className={className}
+                  id={label}
+                  required
+                  placeholder={placeholder}
+                  defaultValue={value}
+                  ref={this.props.compRef}
+                />
+              </span>
             );
         }
         return (
           <span className="inputField">
             <label htmlFor={label}>{label}</label>
-            <textarea id={label} placeholder={placeholder} defaultValue={value} />
+            <textarea
+              className={className}
+              id={label}
+              placeholder={placeholder}
+              defaultValue={value}
+              ref={this.props.compRef}
+            />
           </span>
         );
     }
