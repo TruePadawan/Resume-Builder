0c6e37259634afc8c4e922721ca2243058244d34
Hermes <hermeschigoziri@gmail.com>
2022-06-24 12:41:24 +0100

    build cv data section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Button from "./components/Button/Button";
 import Education from "./components/Education/Education";
 import GeneralInfo from "./components/General Information/GeneralInfo";
 import PracticalExp from "./components/Practical Experience/PracticalExperience";
@@ -6,7 +7,16 @@ import "./css/App.css";
 import "./css/main.css";
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            fullName : "Full Name",
+        }
+    }
+
     render() {
+        const fullName = this.state.fullName;
+
         return (
             <>
                 <h1>CV Builder</h1>
@@ -15,8 +25,20 @@ class App extends Component {
                         <GeneralInfo />
                         <Education />
                         <PracticalExp />
+                        <Button className="generate-pdf">Generate PDF</Button>
                     </section>
-                    <section className="cv-preview"></section>
+                    <section className="cv-preview">
+                        <div className="preview">
+                            <div className="head">
+                                <p className="full-name">{fullName}</p>
+                            </div>
+                            <div className="general">
+                                <div className="contact">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </main>
             </>
         );
